4fd29c0c11d8fbd044c58cb374b9fa5c8ee01858
kris-pakala <37430948+kris-pakala@users.noreply.github.com>
2018-05-05 02:58:52 -0400

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World2!");
+            Console.WriteLine("Hello, World3!");
         }
     }
 }
